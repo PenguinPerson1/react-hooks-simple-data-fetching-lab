cd6c8412c266909fc330a2987021e88fbff6f735
Annania Plossl <nia.plossl@gmail.com>
2024-04-05 12:45:10 -0400

    completed the lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
-// create your App component here
+import React, {useState,useEffect} from "react";
+
+export default function App() {
+    const [isLoaded,setIsLoaded] = useState(false)
+    const [dogImage,setDogImage] = useState("")
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(r=>r.json())
+        .then(image=>{
+            setDogImage(image.message)
+            setIsLoaded(true);
+        })
+        .catch(error=>console.log(error))
+    },[])
+    if (!isLoaded) {
+        return <p>Loading</p>
+    }
+    return <img src={dogImage} alt="A Random Dog"></img>
+}
